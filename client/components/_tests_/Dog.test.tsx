22fddb516b89886ccf7ddd559d304554a7a15231
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-10-30 17:56:31 +1300

    Testing working

--- client/components/_tests_/Dog.test.tsx
+++ client/components/_tests_/Dog.test.tsx
@@ -9,10 +9,30 @@ describe('<Dog /> component', () => {
   test("Renders a dog's name", () => {
     // Arrange
     render(<Dog name="Toodles" breed="Poodle" superpower="Rocket Paws" />)
-  })
-  // Act
 
-  // Assert
-  const heading = screen.getByRole('heading')
-  expect(heading).toHaveTextContent('Toodles')
+    // Act
+
+    // Assert
+    const heading = screen.getByRole('heading')
+    expect(heading).toHaveTextContent('Toodles')
+  })
+
+  test('Renders an appropriate alt tag for an image', () => {
+    // Arrange
+    render(
+      <Dog
+        name="Toodles"
+        breed="Pug"
+        superpower="Rocket Paws"
+        image="/images/pug.png"
+      />
+    )
+    // Act
+
+    // Assert
+    const alt = screen.getByAltText('Toodles is a Pug')
+    // const altText = screen.getByRole('alt')
+    expect(alt).toHaveAttribute('src', '/images/pug.png')
+    // expect(altText).toHaveTextContent('Toodles is a Poodle')
+  })
 })
